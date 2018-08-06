8bdaffae585af04599b1f91c3b2119352160e770
joshpson <jpearson846@gmail.com>
2018-08-06 15:23:45 -0400

    ready for deploy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, Rails.application.credentials.facebook_id,  Rails.application.credentials.facebook_secret
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
 
   get '/friends' => 'friendships#index'
 
-  get  'static/privacy'
+  get  '/privacy' => 'static#privacy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
